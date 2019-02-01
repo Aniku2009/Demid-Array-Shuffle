acbf9c83eeb39ba6002b7160f11eb7fabdcb3539
Aniku2009 <aniku2009g@gmail.com>
2019-02-01 07:20:55 +0200

    fine

--- shufl.js
+++ shufl.js
@@ -2,36 +2,41 @@ const arr = [1,2,3,4,5];
 
 function mainFlow (arrF){
     let arrAfterCopy = copyArr(arrF);
+     // arrAfterCopy.push(10);
     let arrAfterMix = mix(arrAfterCopy);
-
+    let arrAfterCompare = compareElemOfArr(arrAfterCopy, arrAfterMix);
+    console.log(arrAfterCompare);
 }
 
 
 function copyArr (arrayForCopy){
-    let copyOfArr = [...arrayForCopy];
+    let copyOfArr = arrayForCopy.slice();
     return copyOfArr;
 };
 
 
 function mix (arrForMix) {
-    for (let i = arrForMix.length - 1; i > 0; i--) {
+    let mixs = copyArr(arrForMix);
+    for (let i = mixs.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
-        [arrForMix[i], arrForMix[j]] = [arrForMix[j], arrForMix[i]];
+        [mixs[i], mixs[j]] = [mixs[j], mixs[i]];
     }
-    return arrForMix;
+    return mixs;
 }
 
 function compareElemOfArr (arrOrig, arrMix){
     for (let i=0;i<arrOrig.length;i++){
-
+        if (arrOrig[i]===arrMix[i]){
+            shiftEll(arrMix);
+        }
     }
-
+return arrMix;
 }
 
 function shiftEll (arrShif){
-   let copyArr(arrShif);
-   let shifted = arrShif.shift();
-
+   let copy = copyArr(arrShif);
+   let shifted = copy.shift();
+return copy.push(shifted);
 }
 
 mainFlow(arr);

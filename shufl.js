ebe5db0916e0a8305b8307c0d3d30a6a5bbcdb7f
andriyn <Andriy.Nikulshin@nice.com>
2019-02-01 12:51:20 +0200

    working

--- shufl.js
+++ shufl.js
@@ -3,35 +3,51 @@ const arr = [1,2,3,4,5];
 function mainFlow (arrF){
     let arrAfterCopy = copyArr(arrF);
     let arrAfterMix = mix(arrAfterCopy);
-
+    let arrAfterCompare = compareElemOfArr(arrAfterCopy, arrAfterMix);
+    for (let i=0;i<arrAfterCompare.length;i++){
+        console.log(arrAfterCompare[i]);
+        }
+alert('Original array - '+ arrF+'   Mixed array - '+ arrAfterMix+'    Arr after compare - '+arrAfterCompare);
 }
 
 
 function copyArr (arrayForCopy){
-    let copyOfArr = [...arrayForCopy];
+    let copyOfArr = arrayForCopy.slice();
     return copyOfArr;
 };
 
 
 function mix (arrForMix) {
-    for (let i = arrForMix.length - 1; i > 0; i--) {
+    let mixs = copyArr(arrForMix);
+    for (let i = mixs.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
-        [arrForMix[i], arrForMix[j]] = [arrForMix[j], arrForMix[i]];
+        [mixs[i], mixs[j]] = [mixs[j], mixs[i]];
     }
-    return arrForMix;
+    return mixs;
 }
 
 function compareElemOfArr (arrOrig, arrMix){
-    for (let i=0;i<arrOrig.length;i++){
+    // let j=false;
+    // do{
+        for (let i=0;i<arrOrig.length;i++){
+            if (arrOrig[i]==arrMix[i]){
+                let d = shiftEll(arrMix);
+                arrMix = [...d];
+                i=0;
+               //  continue;
+            }
+        }
 
-    }
-
+    // }
+    // while (j=true);
+    return arrMix;
 }
 
 function shiftEll (arrShif){
-   let copyArr(arrShif);
-   let shifted = arrShif.shift();
-
+    let copy = copyArr(arrShif);
+    let shifted = copy.shift();
+    let d =  [...copy, shifted];
+    return d;
 }
 
-mainFlow(arr);
+mainFlow(arr)
